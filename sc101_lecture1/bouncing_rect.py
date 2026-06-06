264e3f64f3796329f8c952bd955763b8d8bf3d23
a1740942002 <a1740942002@me.com>
2026-06-06 16:35:35 +0800

    Remove project configuration files and dependencies from pyproject.toml and .idea directory. Update bouncing_rect.py and my_drawing.py to implement animation and drawing features using the campy graphics library.

--- sc101_lecture1/bouncing_rect.py
+++ sc101_lecture1/bouncing_rect.py
@@ -10,7 +10,7 @@ program shows how animation can be achieved through
 continuous movement and screen refresh.
 """
 
-from campy.graphics.gobjects import GOval
+from campy.graphics.gobjects import GOval, GRect
 from campy.graphics.gwindow import GWindow
 from campy.gui.events.timer import pause
 
@@ -22,8 +22,37 @@ DELAY = 10
 
 
 def main():
-	pass
-			
+	window = GWindow(500, 500)
+
+	rect1 = setup_rect()
+	rect1.location =  (window.width - SIZE) / 2, (window.height - SIZE) / 2
+
+	rect2 = setup_rect()
+	rect2.location =  (window.width - SIZE) / 2, (window.height - SIZE) / 2
+
+	window.add(rect1)
+	window.add(rect2)
+
+	vx = 5
+	vy = 3
+
+	while True:
+		# Update
+		rect1.move(vx, 0)
+		rect2.move(0, vy)
+		# Check
+		if rect1.x <=0 or rect1.x + rect1.width >= window.width :
+			vx = -vx
+		if rect2.y <= 0 or rect2.y +rect2.height >= window.height:
+			vy = -vy
+		# Pause
+		pause(DELAY)
+
+def setup_rect():
+	rect = GRect(SIZE, SIZE)
+	rect.filled = True
+	rect.fill_color = "pink"
+	return rect
 
 if __name__ == '__main__':
 	main()
--- sc101_lecture1/my_drawing.py
+++ sc101_lecture1/my_drawing.py
@@ -1,6 +1,6 @@
 """
 File: my_drawing.py
-Name: 
+Name:
 ----------------------
 This program uses the campy module to
 draw graphical objects on a GWindow.
@@ -10,13 +10,37 @@ the program demonstrates how drawing can
 be done in a graphical window using campy.
 """
 
-from campy.graphics.gobjects import GOval, GRect
+from campy.graphics.gobjects import GOval, GRect, GLabel
 from campy.graphics.gwindow import GWindow
 
 
 def main():
-    pass
+    window = GWindow(800, 500, "MyFace")
+
+    face = GOval(200, 250)
+    face.filled = True
+    face.fill_color = "pink"
+
+    l_eye = GOval(50, 50)
+    l_eye.filled = True
+
+    r_eye = GOval(50, 50)
+    r_eye.filled = True
+
+    mouth = GRect(120, 40)
+    mouth.filled = True
+    mouth.fill_color = "sage"
+
+    label = GLabel("Hello World")
+    label.color = "red"
+    label.font = "-80"
+
+    window.add(face, x=350, y=200)
+    window.add(l_eye, x=390, y=230)
+    window.add(r_eye, x=450, y=230)
+    window.add(mouth, x=390, y=360)
+    window.add(label, x=0, y=label.height)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
