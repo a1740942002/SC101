b8019b243b2ae2f2eca5fde591ef7f7c53318217
a1740942002 <a1740942002@me.com>
2026-06-07 21:12:28 +0800

    assignment 2 ok

--- SC101Assignment1/draw_line.py
+++ SC101Assignment1/draw_line.py
@@ -5,10 +5,20 @@ Name:
 TODO:
 """
 
-from campy.graphics.gobjects import GOval, GLine
+from campy.graphics.gobjects import GOval, GLine, GLabel
 from campy.graphics.gwindow import GWindow
-from campy.gui.events.mouse import onmouseclicked
+from campy.gui.events.mouse import onmouseclicked, onmousemoved
 
+window = GWindow()
+count = 0
+start_x = 0
+start_y = 0
+cursor = None
+
+
+POINTER_SIZE = 25
+pointer = GOval(POINTER_SIZE,POINTER_SIZE)
+window.add(pointer)
 
 def main():
     """
@@ -17,7 +27,33 @@ def main():
     When the user clicks on the canvas for the second time, the circle
     disappears and a line is drawn between the two clicked positions.
     """
-    pass
+    onmouseclicked(on_click)
+    onmousemoved(on_moved)
+
+def on_click(e):
+    global count
+    global start_x
+    global start_y
+    global cursor
+
+    count += 1
+
+    # 奇數
+    if count % 2 == 1:
+        start_x = e.x
+        start_y = e.y
+        cursor = GOval(POINTER_SIZE, POINTER_SIZE)
+        window.add(cursor, x=e.x - cursor.width/2, y=e.y - cursor.height / 2)
+
+    # 偶數
+    else:
+        line = GLine(start_x, start_y, e.x, e.y)
+        window.add(line)
+        if cursor is not None:
+            window.remove(cursor)
+
+def on_moved(e):
+    pointer.location = e.x - pointer.width / 2, e.y - pointer.height/2
 
 
 if __name__ == "__main__":
